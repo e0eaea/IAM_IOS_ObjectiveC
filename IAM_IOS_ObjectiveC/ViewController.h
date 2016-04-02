0ff6b47d5d32427caa30f93b2af29346ede5ff40
e0eaea <e0eaea@gmail.com>
2016-04-02 17:14:28 +0900

    Initial Commit

---
+++ IAM_IOS_ObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAM_IOS_ObjectiveC
+//
+//  Created by KMK on 2016. 4. 2..
+//  Copyright © 2016년 KMK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IAM_IOS_ObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IAM_IOS_ObjectiveC
+//
+//  Created by KMK on 2016. 4. 2..
+//  Copyright © 2016년 KMK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
